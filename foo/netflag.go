28aa728844db53cacc9b9262a2a4f2cbac57b6f8
Ted <ski2per@163.com>
2020-06-19 19:18:46 +0800

    go home

---
+++ foo/netflag.go
@@ -0,0 +1,14 @@
+package main
+
+type Flags uint
+
+const (
+	FlagUp Flags = 1 << iota
+	FlagBroadcast
+	FlagLoopback
+	FlagPointToPoint
+	FlagMulticast
+)
+
+func IsUp(v Flags) bool { return v&FlagUp == FlagUp }
+func TurnDown(v *Flags)
---
+++ foo/str.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "hello, Ted"
+	fmt.Println(len(s))
+	fmt.Println(s[0], s[7])
+}
