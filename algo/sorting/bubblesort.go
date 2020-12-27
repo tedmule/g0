7e9a74f13c7dac404461de01e357bdfca3eedf4a
Ted <ski2per@163.com>
2020-12-27 11:19:47 +0800

    update bubble sort in go

--- algo/sorting/bubblesort.go
+++ algo/sorting/bubblesort.go
@@ -19,19 +19,33 @@ func BubbleSort2(numbers []int) []int {
 	n := len(numbers)
 	// Outer loop starts from 0 to length-1
 	for i := 0; i < n; i++ {
-		// Innter loop also starts from 0,
+		// Inner loop also starts from 0,
 		// so we CHOOSE j+1 for the adjacent element,
 		// and use n-i-1 to prevent array from OutOfIndex
-		for j := 0; j < (n - i - 1); i++ {
+		for j := 0; j < (n - i - 1); j++ {
 			if numbers[j] > numbers[j+1] {
-				tmp := numbers[j]
-				numbers[j] = numbers[j+1]
-				numbers[j+1] = tmp
+				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
 			}
 		}
-
+		// fmt.Println(numbers)
 	}
 	return numbers
 }
 
-func BubbleSortOptimized(numbers []int) {}
+func BubbleSortOptimized(numbers []int) []int {
+	n := len(numbers)
+	for i := 0; i < n; i++ {
+		swapped := false
+		for j := 0; j < (n - i - 1); j++ {
+			if numbers[j] > numbers[j+1] {
+				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+				swapped = true
+			}
+		}
+		// fmt.Println(numbers)
+		if !swapped {
+			break
+		}
+	}
+	return numbers
+}
