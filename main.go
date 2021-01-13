d3f13f9132893e5a0237ebcf8422dedb977205af
Ted <ski2per@gmail.com>
2021-01-13 22:16:20 +0800

    add partition sort

--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 
 	"github.com/gog0/abc"
-	"github.com/gog0/algo/searching"
 	"github.com/gog0/algo/sorting"
 )
 
 func main() {
 	arr := abc.GenerateRandIntArr(10)
 	fmt.Printf("Origin: %+v\n", arr)
+
 	defer abc.Elapsed()()
-	sorted := sorting.BubbleSort(arr)
+
+	// sorted := sorting.BubbleSort(arr)
+	// sorted := sorting.QuickSort(arr)
+	sorted := sorting.QuickSort2(arr)
+
 	fmt.Printf("Sorted: %v\n", sorted)
 
-	searching.BinarySearch()
 }
